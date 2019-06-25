12a5c29bdb21a2776784a2fdf8afe50da4ca9b5a
aafomin <aafomin@itps-russia.ru>
2019-06-25 14:11:24 +0500

    fix firebase config 2

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,9 @@
-import firebase from 'firebase'
-// import firebase from 'firebase/app';
+// import firebase from 'firebase'
+import firebase from 'firebase/app';
+import '@firebase/auth';
+import 'firebase/firestore'
+import 'firebase/storage'
+
 const config = {
   apiKey: "AIzaSyAJiJqj-00ZD8KVrmIa4x9BuguEKSG7oDY",
   authDomain: "routesite-vue.firebaseapp.com",
@@ -8,7 +12,7 @@ const config = {
   storageBucket: "routesite-vue.appspot.com",
   messagingSenderId: "567281260307"
 };
-// export default config
+
 export const firebaseApp = firebase.initializeApp(config);
 export const db = firebaseApp.firestore();
 export const storage = firebase.storage()
