e7ef4dfb7170c8f1cc9c6285e644cda21b45d968
aafomin <aafomin@itps-russia.ru>
2019-07-12 15:52:46 +0500

    add onTop button in News

--- src/Components/News/NewsPage.js
+++ src/Components/News/NewsPage.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import NewsCardItem from "./NewsCardItem";
 import NewsCategoryMenu from "./NewsCategory";
+import Fab from "@material-ui/core/Fab";
+import ArrowUp from "@material-ui/icons/KeyboardArrowUp";
 
 const NewsPage = props => {
   const stylus = {
@@ -9,15 +11,53 @@ const NewsPage = props => {
       paddingTop: "1rem"
     },
     menu: {
-      position: "absolute",
+      position: "fixed",
       padding: "0.5rem",
+      zIndex: 1
+    },
+    button: {
+      marginTop: "20%",
+      position: "fixed",
       zIndex: 1
     }
   };
+  const onTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  const [onTopButton, setOnTopButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 350) {
+        if (!onTopButton) {
+          setOnTopButton(true);
+        }
+      } else if (onTopButton) {
+        setOnTopButton(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [onTopButton]);
+
   return (
     <div className="container" style={stylus.container}>
       <div className="menu" style={stylus.menu}>
         <NewsCategoryMenu />
+      </div>
+      <div className="up" style={stylus.button}>
+        {onTopButton && (
+          <Fab color="primary" aria-label="Up" onClick={() => onTop()}>
+            <ArrowUp />
+          </Fab>
+        )}
       </div>
       {props.news.map((el, index) => (
         <NewsCardItem news={el} key={index} />
@@ -26,9 +66,7 @@ const NewsPage = props => {
   );
 };
 
-// ToDo
 NewsPage.propTypes = {
   news: PropTypes.array.isRequired
-  // func.isRequired
 };
 export default NewsPage;
