398f5d896d7ff85fbf9bd64fc74e85a613778375
aafomin <alexcode@mail.ru>
2019-12-03 11:50:53 +0500

    refactoring image helper

--- src/Components/Helpers/Helper.js
+++ src/Components/Helpers/Helper.js
@@ -1,51 +1,52 @@
-export const resizeImage = function (settings) {
-  let file = settings.file;
-  let maxSize = settings.maxSize;
-  let reader = new FileReader();
-  let image = new Image();
-  let canvas = document.createElement('canvas');
-  let dataURItoBlob = function(dataURI) {
-    let bytes = dataURI.split(',')[0].indexOf('base64') >= 0 ?
-      atob(dataURI.split(',')[1]) :
-      unescape(dataURI.split(',')[1]);
-    let mime = dataURI.split(',')[0].split(':')[1].split(';')[0];
-    let max = bytes.length;
-    let ia = new Uint8Array(max);
-    for (let i = 0; i < max; i++)
-      ia[i] = bytes.charCodeAt(i);
+export const resizeImage = function(settings) {
+  const { file } = settings;
+  const { maxSize } = settings;
+  const reader = new FileReader();
+  const image = new Image();
+  const canvas = document.createElement("canvas");
+  const dataURItoBlob = function(dataURI) {
+    const bytes =
+      dataURI.split(",")[0].indexOf("base64") >= 0
+        ? atob(dataURI.split(",")[1])
+        : unescape(dataURI.split(",")[1]);
+    const mime = dataURI
+      .split(",")[0]
+      .split(":")[1]
+      .split(";")[0];
+    const max = bytes.length;
+    const ia = new Uint8Array(max);
+    for (let i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
     return new Blob([ia], { type: mime });
   };
-  let resize = function() {
-    let width = image.width;
-    let height = image.height;
+  const resize = function() {
+    let { width } = image;
+    let { height } = image;
     if (width > height) {
       if (width > maxSize) {
         height *= maxSize / width;
         width = maxSize;
       }
-    } else {
-      if (height > maxSize) {
-        width *= maxSize / height;
-        height = maxSize;
-      }
+    } else if (height > maxSize) {
+      width *= maxSize / height;
+      height = maxSize;
     }
     canvas.width = width;
     canvas.height = height;
-    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
-    let dataUrl = canvas.toDataURL('image/jpeg');
+    canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+    const dataUrl = canvas.toDataURL("image/jpeg");
     return dataURItoBlob(dataUrl);
   };
-  return new Promise(function(ok, no) {
+  return new Promise((ok, no) => {
     if (!file.type.match(/image.*/)) {
       no(new Error("Not an image"));
       return;
     }
     reader.onload = function(readerEvent) {
-      image.onload = function() { return ok(resize()); };
+      image.onload = function() {
+        return ok(resize());
+      };
       image.src = readerEvent.target.result;
     };
     reader.readAsDataURL(file);
   });
-}
-
-
+};
