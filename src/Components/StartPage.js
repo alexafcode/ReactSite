80bc33f9fcd46e079d81717326b1780d301ed7f7
aafomin <alexcode@mail.ru>
2020-12-08 09:16:02 +0500

    fix start page

--- src/Components/StartPage.js
+++ src/Components/StartPage.js
@@ -1,15 +1,25 @@
 import React from "react";
+
 function StartPage() {
+  const width =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  const height =
+    window.innerHeight ||
+    document.documentElement.clientHeight ||
+    document.body.clientHeight;
+
   const stylus = {
     main: {
       width: "100%",
-      height: "93vh",
-      backgroundImage: "url(https://picsum.photos/1000/600?random)",
+      height: height - 64,
+      backgroundImage: `url(https://picsum.photos/${width}/${height}?random)`,
       backgroundSize: "cover",
       backgroundPosition: "center",
       backgroundRepeat: "no-repeat",
-      overflow: "hidden"
-    }
+      overflow: "hidden",
+    },
   };
   return <div className="main" style={stylus.main} />;
 }
